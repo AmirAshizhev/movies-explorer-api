e1b0303c0492faffb3736d041ad4c3748fe34272
Amir Ashizhev <amirashizhev@yandex.ru>
2022-09-11 14:00:18 +0300

    schemes and models

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Невалидный адресс электронной почты',
+    },
+  },
+  password: {
+    type: String,
+    require: true,
+    // select: false,
+  },
+});
+
+module.exports.User = mongoose.model('user', userSchema);
